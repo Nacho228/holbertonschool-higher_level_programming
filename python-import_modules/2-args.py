f403d8aa18c73050d4e7e61cf980b81339d4a78f
Nacho228 <6319@holbertonstudents.com>
2023-06-04 19:02:57 -0700

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
-        print("1 argument\n1: {}".format(sys.argv[1]))
+        print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-        for i, arg in enumerate(sys.argv):
-            print(f"{i:0}: {arg}")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"{i:}: {arg}")
