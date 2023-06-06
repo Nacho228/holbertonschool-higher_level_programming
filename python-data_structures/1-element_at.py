f9f1404934c11044ac0dff27ff6b67055cd4288e
Nacho228 <6319@holbertonstudents.com>
2023-06-06 06:31:27 -0700

    there is no way that this is wrong

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx in my_list or idx in range(my_list):
+    if idx in my_list:
         return my_list[idx]
+    elif idx in range(my_list):
+        return my_list[idx]
+    else:
+        return
