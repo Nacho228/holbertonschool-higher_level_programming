fa9fe170678f7d7ed3fa8f343ef7818f2bed88c8
Nacho228 <6319@holbertonstudents.com>
2023-06-06 10:42:39 -0700

    Handlingerrors

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     copy_list = my_list.copy()
     if idx not in range(len(my_list) - 1) or idx < 0:
-        return copy_list
+        return my_list
     else:
         copy_list[idx] = element
         return copy_list
