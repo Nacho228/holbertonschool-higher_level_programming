662637daaad98906c11cafaeb9b90d9533ea80fd
Nacho228 <6319@holbertonstudents.com>
2023-06-07 11:27:04 -0700

    tomfoolery

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in x:
-            if y != x[-1]: 
+            if y != x[-1]:
                 print("{}".format(y), end=" ")
             else:
                 print("{}".format(y), end="")
