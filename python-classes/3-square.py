c07a46c8388a3855b275f0608303d96b1c87543e
Nacho228 <6319@holbertonstudents.com>
2023-06-14 09:37:14 -0700

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""tarea toomfoolery"""
+
+
+class Square:
+    """
+        A Square class that handles certain errors of certain class
+        (or not) attributes (or not).
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
