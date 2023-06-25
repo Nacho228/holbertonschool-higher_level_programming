614829a30a0cd67aa43fee153f9ac0aef9b6a5d8
Nacho228 <6319@holbertonstudents.com>
2023-06-25 16:56:21 -0700

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,15 @@
 
 
 def write_file(filename="", text=""):
+    """_summary_
+
+    Args:
+        filename: name of the file to be written on. Defaults to "".
+        text: text to be written with. Defaults to "".
+
+    Returns:
+        Number of characters written
+    """
     with open(filename, "w+", encoding="utf-8") as f:
         written = f.write(text)
         return written
