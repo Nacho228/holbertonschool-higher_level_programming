ed86a8f255ac4aa43e1e884db0c132bbce94fb3a
Nacho228 <6319@holbertonstudents.com>
2023-06-25 15:09:30 -0700

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,8 +4,4 @@
 
 def inherits_from(obj, a_class):
     """ is same class """
-    if not type(obj) is a_class:
-        if isinstance(obj, a_class):
-            return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) != a_class
