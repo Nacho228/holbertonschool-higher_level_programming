8fffc8749933f25d3f4ac0b53dfac51d4be0684f
Nacho228 <6319@holbertonstudents.com>
2023-06-25 17:03:17 -0700

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Task 2"""
+
+
+def append_write(filename="", text=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+    """
+    with open(filename, "a+", encoding="utf-8") as f:
+         appended = f.write(text)
+         return appended
