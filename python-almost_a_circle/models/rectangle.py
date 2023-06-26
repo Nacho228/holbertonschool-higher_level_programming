b47d6dad0a9f4a598c07159deb5a8a33af210688
Nacho228 <6319@holbertonstudents.com>
2023-06-26 11:13:27 -0700

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,51 +16,59 @@ class Rectangle(Base):
             y (int, optional): Height number. Defaults to 0.
             id inherited from super().
         """
-        __width = width
-        __height = height
-        __x = x
-        __y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
 
 "Getters and setter"
 
 
+@property
 def width(self):
     """ width getter """
     return self.__width
 
 
+@width.setter
 def width(self, value):
     """width setter"""
     self.__width = value
 
 
+@property
 def height(self):
     """height getter"""
     return self.__height
 
 
+@height.setter
 def height(self, value):
     """heigth setter"""
     self.__height = value
 
 
+@property
 def x(self):
     """x getter"""
     return self.__x
 
 
+@x.setter
 def x(self, value):
     """x setter"""
     self.__x = value
 
 
+@property
 def y(self):
     """y getter"""
     return self.__y
 
 
+@y.setter
 def y(self, value):
     """y setter"""
     self.__y = value
