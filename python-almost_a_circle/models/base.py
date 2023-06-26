3736fd5d72bb76ee3458079c51401584ca7f02d3
Nacho228 <6319@holbertonstudents.com>
2023-06-26 10:02:54 -0700

    models/base.py and models/__init__.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" task 1 python almost a circle"""
+
+
+class Base:
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initialization of id"""
+
+        __nb_objects = 0
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
