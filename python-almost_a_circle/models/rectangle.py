bbe3d07c58478ed67b2cd9d094619317ae357e4d
Nacho228 <6319@holbertonstudents.com>
2023-06-29 11:44:13 -0700

    Square CLASSS

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -57,8 +57,9 @@ class Rectangle(Base):
         Returns:
             Rectangle and its attributes
         """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height
+        return "[{}] ({}) {}/{} - {}/{}".format(
+            self.__class__.__name__, self.id, self.__x, self.__y,
+            self.__width, self.__height
         )
 
     def update(self, *args, **kwargs):
---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""New class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from
+    rectangle.
+
+    Args:
+        Square: with all attributes. 
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        return super().__str__()
+
+
